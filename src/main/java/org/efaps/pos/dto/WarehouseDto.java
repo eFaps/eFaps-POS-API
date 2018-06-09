d91d3a957628ae106c4dc44d8f8e6b07fb3ca370
Jan Moxter <jan.moxter@innobix.com>
2018-06-09 15:37:54 -0500

    Adding WarehouseDto

---
+++ src/main/java/org/efaps/pos/dto/WarehouseDto.java
@@ -0,0 +1,46 @@
+package org.efaps.pos.dto;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+@JsonDeserialize(builder = WarehouseDto.Builder.class)
+public class WarehouseDto
+    extends AbstractDto
+{
+
+    private final String name;
+
+    private WarehouseDto(final Builder _builder)
+    {
+        super(_builder);
+        this.name = _builder.name;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public static Builder builder()
+    {
+        return new Builder();
+    }
+
+    public static final class Builder
+        extends AbstractDto.Builder<Builder, WarehouseDto>
+    {
+
+        private String name;
+
+        public Builder withName(final String _name)
+        {
+            this.name = _name;
+            return this;
+        }
+
+        @Override
+        public WarehouseDto build()
+        {
+            return new WarehouseDto(this);
+        }
+    }
+}
