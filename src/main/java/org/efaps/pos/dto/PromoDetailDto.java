197cf0daeec5a3fedf3b1c42e7a6cb334b05d0d3
Jan Moxter <jan.moxter@moxter.net>
2025-03-08 12:16:38 -0500

    Ignore

--- src/main/java/org/efaps/pos/dto/PromoDetailDto.java
+++ src/main/java/org/efaps/pos/dto/PromoDetailDto.java
@@ -19,6 +19,7 @@ import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 @JsonDeserialize(builder = PromoDetailDto.Builder.class)
@@ -126,6 +127,7 @@ public class PromoDetailDto
         return new Builder();
     }
 
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static final class Builder
     {
 
