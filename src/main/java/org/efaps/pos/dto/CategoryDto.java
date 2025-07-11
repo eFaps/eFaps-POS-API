8f21a2b5b3bfd375da27c8cae94a665e3e908356
Jan Moxter <jan.moxter@moxter.net>
2025-07-11 14:29:37 -0500

    Category file modifiedAt

--- src/main/java/org/efaps/pos/dto/CategoryDto.java
+++ src/main/java/org/efaps/pos/dto/CategoryDto.java
@@ -15,6 +15,8 @@
  */
 package org.efaps.pos.dto;
 
+import java.time.OffsetDateTime;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
@@ -32,6 +34,8 @@ public class CategoryDto
 
     private final String imageOid;
 
+    private final OffsetDateTime imageModifiedAt;
+
     private final String parentOid;
 
     public CategoryDto(final Builder builder)
@@ -41,6 +45,7 @@ public class CategoryDto
         description = builder.description;
         weight = builder.weight;
         imageOid = builder.imageOid;
+        imageModifiedAt = builder.imageModifiedAt;
         parentOid = builder.parentOid;
     }
 
@@ -62,6 +67,11 @@ public class CategoryDto
     public String getImageOid()
     {
         return imageOid;
+    }
+
+    public OffsetDateTime getImageModifiedAt()
+    {
+        return imageModifiedAt;
     }
 
     public String getParentOid()
@@ -96,6 +106,7 @@ public class CategoryDto
         private String description;
         private int weight = 0;
         private String imageOid;
+        private OffsetDateTime imageModifiedAt;
         private String parentOid;
 
         public Builder withName(final String _name)
@@ -119,6 +130,12 @@ public class CategoryDto
         public Builder withImageOid(final String _imageOid)
         {
             imageOid = _imageOid;
+            return this;
+        }
+
+        public Builder withImageModifiedAt(final OffsetDateTime imageModifiedAt)
+        {
+            this.imageModifiedAt = imageModifiedAt;
             return this;
         }
 
