478427587ba475af64a5362bfa39e5f525dddff8
Jan Moxter <jan.moxter@moxter.net>
2026-05-18 11:17:14 -0500

    Jackson 3

--- src/main/java/org/efaps/pos/dto/ProductDto.java
+++ src/main/java/org/efaps/pos/dto/ProductDto.java
@@ -26,7 +26,9 @@ import org.efaps.pos.interfaces.IExtendable;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import tools.jackson.databind.annotation.JsonDeserialize;
+
 
 @JsonDeserialize(builder = ProductDto.Builder.class)
 @JsonIgnoreProperties(ignoreUnknown = true)
